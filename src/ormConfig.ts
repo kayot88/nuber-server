d250a61a2c9d91c0e7ac079391b032455ba85d48
Kayot88 <kayot88@gmail.com>
2020-01-04 00:22:25 +0200

    remove passw

---
+++ src/ormConfig.ts
@@ -0,0 +1,18 @@
+import { ConnectionOptions } from "typeorm";
+
+const connectionOptions: ConnectionOptions = {
+  type: "postgres",
+  database: "nuber",
+  synchronize: true,
+  logging: true,
+  entities: ["src/entities/**/*.ts"],
+  host: process.env.DB_ENDPOINT || "localhost",
+  port: 5432,
+  username: process.env.DB_USERNAME || "postgres",
+  password: process.env.DB_PASSWORD || ""
+
+  // migrations: ["src/migration/**/*.ts"],
+  // subscribers: ["src/subscriber/**/*.ts"]
+};
+
+export default connectionOptions;
