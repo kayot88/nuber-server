7432294a83b77ce66987d0d94b1bf7a94bc1a5f9
Kayot88 <kayot88@gmail.com>
2020-02-04 21:33:21 +0200

    DeletePlace

---
+++ src/api/Place/DeletePlace/DeletePlace.resolvers.ts
@@ -0,0 +1,50 @@
+import { Resolvers } from "src/types/resolvers";
+import { privatResolver } from "src/utils/privatResolver";
+import User from "src/entities/User";
+import Place from "./../../../entities/Place";
+import {
+  DeletePlaceMutationArgs,
+  DeletePlaceResponse
+} from "./../../../types/graphql.d";
+const resolvers: Resolvers = {
+  Mutation: {
+    DeletePlace: privatResolver(
+      async (
+        _,
+        args: DeletePlaceMutationArgs,
+        { req }
+      ): Promise<DeletePlaceResponse> => {
+        const user: User = req.user;
+        try {
+          const place = await Place.findOne({ id: args.placeId });
+          if (place) {
+            if (place.userId === user.id) {
+              await place.remove();
+              return {
+                ok: true,
+                error: null
+              };
+            } else {
+              return {
+                ok: false,
+                error: "No user found"
+              };
+            }
+          } else {
+            return {
+              ok: false,
+              error: "No place found"
+            };
+          }
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
