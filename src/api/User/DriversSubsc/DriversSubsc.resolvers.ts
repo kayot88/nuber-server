06bb208c49f314e9e844db901cf6e5e03a72c97d
Kayot88 <kayot88@gmail.com>
2020-02-06 13:43:25 +0200

    withFilter(filtering subscr messages_p1)

--- src/api/User/DriversSubsc/DriversSubsc.resolvers.ts
+++ src/api/User/DriversSubsc/DriversSubsc.resolvers.ts
@@ -1,9 +1,26 @@
+import { withFilter } from "graphql-yoga";
+import User from "./../../../entities/User";
+
 const resolvers = {
   Subscription: {
     DriverSubsc: {
-      subscribe: (_, __, { pubSub }) => {
-        return pubSub.asyncIterator("driverUpdate");
-      }
+      subscribe: withFilter(
+        (_, __, { pubSub }) => pubSub.asyncIterator("driverUpdate"),
+        (payload, _, { connectionContext }) => {
+          const user: User = connectionContext.currentUser;
+          const {
+            DriverSubsc: { lastLat: driverLastLat, lastLng: driverLastLng }
+          } = payload;
+          const { lastLat: userLastLat, lastLng: userLastLng } = user;
+          return (
+            driverLastLat >= userLastLat - 0.05 &&
+            driverLastLat <= userLastLat + 0.05 &&
+            driverLastLng >= userLastLng - 0.05 &&
+            driverLastLng <= userLastLng + 0.05
+          );
+          // return true;
+        }
+      )
     }
   }
 };
